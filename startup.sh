c0093631a4a9e59135443a0fd25345ff79cede02
Paimpozhil <paimpozhil@gmail.com>
2014-08-31 18:01:19 +0000

    testing xvfb

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-x11vnc -create -forever -ncache 10 -ncache_cr  &
+export DISPLAY=:1
+Xvfb :1 -screen 0 1280x1024x16 &
+openbox-session&
+x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
