775fdbdf1484121b216056b2b1b5b99948dcfdbd
Sean Payne <seantpayne@gmail.com>
2015-05-17 01:14:30 -0700

    Changed virtual screen dimensions

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export DISPLAY=:1
-Xvfb :1 -screen 0 1280x1024x16 &
+Xvfb :1 -screen 0 1600x900x16 &
 openbox-session&
 x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
