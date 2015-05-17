05a3fdeabc56f00b586f8c1d64b3476f7c04accc
Sean Payne <seantpayne@gmail.com>
2015-05-17 01:15:04 -0700

    Added short delay to ensure Xvfb created framebuffer

--- startup.sh
+++ startup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
+sleep 5
 openbox-session&
 x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
