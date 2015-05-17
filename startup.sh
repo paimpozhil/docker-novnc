8bccad38f11f39c1c666caab403f7894d919341c
Sean Payne <seantpayne@gmail.com>
2015-05-17 01:49:52 -0700

    Changed index to automatically connect to the novnc instance.

--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@ Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 openbox-session&
 x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
-cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
+cd /root/noVNC && ln -s vnc_auto.html index.html && ./utils/launch.sh --vnc localhost:5900
