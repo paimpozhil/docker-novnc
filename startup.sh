4a0cbfba00f4dd015db22aa590cfe04285713005
Sean Payne <seantpayne@gmail.com>
2015-05-17 01:15:44 -0700

    Changed x11vnc options to allow reconnections

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ export DISPLAY=:1
 Xvfb :1 -screen 0 1600x900x16 &
 sleep 5
 openbox-session&
-x11vnc -display :1 -bg -nopw -listen localhost -xkb -ncache 10 -ncache_cr
+x11vnc -display :1 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 cd /root/noVNC && ./utils/launch.sh --vnc localhost:5900
